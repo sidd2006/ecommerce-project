b955028116ed12d3f22bd6e8357591760dd8b936
sidd2006 <89688501+sidd2006@users.noreply.github.com>
2025-08-27 07:14:57 +0000

    locally vs gloabally

--- ecommerce-project/vite.config.js
+++ ecommerce-project/vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/ecommerce-project/',   
+  base: process.env.NODE_ENV === 'production' ? '/ecommerce-project/' : '/',
 })
